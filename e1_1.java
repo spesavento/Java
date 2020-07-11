bab740cd8e2a1d001bc0e6fd07885c05ecb40e44
Sara Pesavento <spesavento@users.noreply.github.com>
2020-07-10 17:14:44 -0700

    new Java exercises

---
+++ e1_1.java
@@ -0,0 +1,7 @@
+//Exercise 1.1: Write a program that prints out Hellos in any language you'd like
+
+public class e1_1 {
+    public static void main(String[] args) {
+        System.out.println("Hello Annyeong");
+    }
+} 
