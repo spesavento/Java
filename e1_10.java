df9733d2ea80fb07bc1e2e923ebc53762e448cb0
Sara Pesavento <spesavento@users.noreply.github.com>
2020-07-10 20:34:25 -0700

    Chapter 1 Exercises

---
+++ e1_10.java
@@ -0,0 +1,13 @@
+//Exercise 1.10: Write a program that prints a house that looks exactly like the following.
+
+public class e1_10 {
+    public static void main(String[] args) {
+        System.out.println("   +   ");
+        System.out.println("  + +  ");
+        System.out.println(" +   + ");
+        System.out.println("+-----+");
+        System.out.println("| .-. |");
+        System.out.println("| | | |");
+        System.out.println("+-+-+-+");
+    }
+}
---
+++ e1_9.java
@@ -0,0 +1,11 @@
+/* Exercise 1.9: Write a program that prints out an imitation of a Piet Mondrian painting.
+Use character sequences like @@@ or ::: to indicate different colors and - and | for lines. */
+
+public class e1_9 {
+    public static void main(String[] args) {
+        System.out.println("@@@@@@@@@@@@$::::::::::::\n@@@@@@@@@@@@$::::::::::::\n@@@@@@@@@@@@$::::::::::::\n@@@@@@@@@@@@$::::::::::::\n$$$$$$$$$$$$$$$$$$$$$$$$$");
+        System.out.println("::::::::::::$::::::::::::\n::::::::::::$::::::::::::\n$$$$$$$$$$$$$$$$$$$$$$$$$");
+        System.out.println("***$::::::::$######$:::::\n***$::::::::$######$:::::\n***$::::::::$$$$$$$$:::::\n***$::::::::$::::::$:::::");
+    }
+    
+}
